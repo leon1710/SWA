b0a0e450de8cca5b4d06050c1f08ed657e936c55
leon1710 <leon.mueller92@gmail.com>
2016-11-18 22:32:41 +0100

    timer set, button disabled when clicking on stopgenbtn

--- LKW_Liste/ViewModel/MainViewModel.cs
+++ LKW_Liste/ViewModel/MainViewModel.cs
@@ -2,6 +2,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using System.Collections.ObjectModel;
 using System;
+using System.Windows.Threading;
 
 namespace LKW_Liste.ViewModel
 {
@@ -10,8 +11,9 @@ namespace LKW_Liste.ViewModel
         private ObservableCollection<TruckVM> waitingTrucks = new ObservableCollection<TruckVM>();
         private ObservableCollection<TruckVM> readyTrucks = new ObservableCollection<TruckVM>();
         private ObservableCollection<LoadVM> load = new ObservableCollection<LoadVM>();
+        private DispatcherTimer timer = new DispatcherTimer();
+        
 
-    
         private TruckVM selectedReadyTruck;
         private TruckVM selectedWaitingTruck;
         private RelayCommand deleteBtnClicked;
@@ -23,8 +25,21 @@ namespace LKW_Liste.ViewModel
         {
             LoadData();
             DeleteBtnClicked = new RelayCommand(ClearAllEntries);
-            StartGenBtnClicked = new RelayCommand(CreateEntries);
+            StartGenBtnClicked = new RelayCommand(CreateEntries, CheckIfBtnClicked);
+            StopGenBtnClicked = new RelayCommand(StopGenerating);
             shiftTruckToReady = new RelayCommand(ShiftToReadyTrucks);
+        }
+
+        private void StopGenerating()
+        {
+            Timer.Stop();
+        }
+
+        private bool CheckIfBtnClicked()
+        {
+            if (WaitingTrucks.Count > 0)
+                return false;
+            else return true;
         }
 
         private void ShiftToReadyTrucks()
@@ -36,6 +51,21 @@ namespace LKW_Liste.ViewModel
 
         private void CreateEntries()
         {
+            Timer.Interval = new TimeSpan(0, 0, 3);
+            Timer.Tick += new EventHandler(CreateEntriesWithTimer);
+            Timer.Start();
+            /*
+            WaitingTrucks.Add(new TruckVM("Salzburg", 2));
+            WaitingTrucks[0].Load.Add(new LoadVM("Paket1", 3, 3));
+            WaitingTrucks.Add(new TruckVM("Wien", 5));
+            WaitingTrucks[1].Load.Add(new LoadVM("Paket2", 2, 5));
+            WaitingTrucks.Add(new TruckVM("Brüssel", 3));
+            WaitingTrucks[2].Load.Add(new LoadVM("Paket3", 2, 5));
+            */
+        }
+
+        private void CreateEntriesWithTimer(object sender, EventArgs e)
+        {
             WaitingTrucks.Add(new TruckVM("Salzburg", 2));
             WaitingTrucks[0].Load.Add(new LoadVM("Paket1", 3, 3));
             WaitingTrucks.Add(new TruckVM("Wien", 5));
@@ -44,12 +74,6 @@ namespace LKW_Liste.ViewModel
             WaitingTrucks[2].Load.Add(new LoadVM("Paket3", 2, 5));
         }
 
-        /*
-        private bool CheckIfBtnEnabled()
-        {
-            return WaitingTrucks.Count > 1;
-        }
-        */
         private void LoadData()
         {
             // waitingTrucks.Add(new TruckVM("Salzburg", 2, )
@@ -145,6 +169,7 @@ namespace LKW_Liste.ViewModel
             set
             {
                 selectedWaitingTruck = value;
+                //RaisePropertyChanged();
             }
         }
 
@@ -158,6 +183,19 @@ namespace LKW_Liste.ViewModel
             set
             {
                 load = value;
+            }
+        }
+
+        public DispatcherTimer Timer
+        {
+            get
+            {
+                return timer;
+            }
+
+            set
+            {
+                timer = value;
             }
         }
     }
